f648c539ff86af07a76ac584bbb0b820f508f18d
DawnMagnet <axccjqh@qq.com>
2021-10-07 23:54:03 +0800

    add record

--- record.sh
+++
@@ -1,4 +0,0 @@
-for f in $(find  .  -type f -regex  ".*.html")
-do
-    sed -i '31 a <script>\nvar _hmt = _hmt || [];\n(function() {\n  var hm = document.createElement("script");\n  hm.src = "https://hm.baidu.com/hm.js?68e5a42781c7cf226eb26bac2cf1a765";\n  var s = document.getElementsByTagName("script")[0]; \n  s.parentNode.insertBefore(hm, s);\n})();\n</script>' $f
-done
